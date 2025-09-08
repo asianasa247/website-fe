76f7f0b9461d12b1313fffaa003688500f0f6eae
asianasa247 <65910911+asianasa247@users.noreply.github.com>
2025-09-08 15:08:22 +0700

    c
    c

--- src/app/[locale]/(marketing)/api/api.ts
+++ src/app/[locale]/(marketing)/api/api.ts
@@ -30,10 +30,8 @@ api.interceptors.request.use((config) => {
 
   if (token) {
     config.headers = config.headers ?? {};
-   (config.headers as Record<string, string>).Authorization = `Bearer ${dbName}`;
-    (config.headers as Record<string, string>).dbName = `dbName`;
-    //(config.headers as Record<string, string>).Authorization = `Bearer ${token}`;
-    //(config.headers as Record<string, string>).dbName = `${dbName}`;
+    (config.headers as Record<string, string>).Authorization = `Bearer ${token}`;
+    (config.headers as Record<string, string>).dbName = `${dbName}`;
   }
   return config;
 });
